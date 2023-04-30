2d59424dfa326c40ecb44ee473b3d70edfbb50c3
aakj2010 <aakj2010@gmail.com>
2023-04-30 19:12:14 +0530

    tamil-Quran updated

--- src/Tamil Quran/TamilQuran.jsx
+++ src/Tamil Quran/TamilQuran.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useState } from 'react'
 import FontContext from '../context/FontContext';
 import SideBarContext from '../context/SideBarContext';
 import Tab from '../screen/Tab';
-import TamilQuran1 from './TamilQuran1';
+import data from './tam-quran.json'
+import TamilContent from './TamilContent';
 
 const TamilQuran = () => {
-    
+
     let context = useContext(FontContext);
     let SideBarcontext = useContext(SideBarContext);
 
@@ -24,45 +25,28 @@ const TamilQuran = () => {
                 <div className={!SideBarcontext.open ? 'chapter-list-wrapper' : 'chapter-list-relative'}>
                     <div className='tab-container' style={{ fontSize: `${context.fontSize}px` }}>
                         <div className='tabs' style={{ fontSize: `${context.fontSize}px` }}>
-                            <Tab
-                                label="1. வெற்றிக்கு மேல் வெற்றி"
-                                isActive={activeTab === 0}
-                                onClick={() => handleTabClick(0)}
-                            />
-                            <div className='divider-chapter'></div>
-                            <Tab
-                                label="2. பசு"
-                                isActive={activeTab === 1}
-                                onClick={() => handleTabClick(1)}
-                            />
-                            <div className='divider-chapter'></div>
-                            <Tab
-                                label="3. இம்ரானின் குடும்பத்தினர்"
-                                isActive={activeTab === 2}
-                                onClick={() => handleTabClick(2)}
-                            />
-                            <div className='divider-chapter'></div>
-                            <Tab
-                                label="4.பெண்கள்"
-                                isActive={activeTab === 3}
-                                onClick={() => handleTabClick(3)}
-                            />
-                            <div className='divider-chapter'></div>
-                            <Tab
-                                label="5.கால்நடைகள்"
-                                isActive={activeTab === 4}
-                                onClick={() => handleTabClick(4)}
-                            />
+                            {
+                                data.chapters.map((chapter, index) => (
+                                    <>
+                                        <Tab
+                                            label={`${chapter.number}. ${chapter.title}`}
+                                            isActive={activeTab === index}
+                                            onClick={() => handleTabClick(index)}
+                                        />
+                                        <div className='divider-chapter'></div>
+                                    </>
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
                 <div className="tab-content" style={{ fontSize: `${context.fontSize}px` }}>
-                    {activeTab === 0 && <TamilQuran1 />}
-                    {activeTab === 1 && <TamilQuran1 />}
-                    {activeTab === 2 && <p>Content for Tab 3</p>}
-                    {activeTab === 3 && <TamilQuran1 />}
-                    {activeTab === 4 && <TamilQuran1 />}
+                    {[...Array(114)].map((_, i) => {
+                        return activeTab === i && <TamilContent index={i + 1} />
+                    })}
                 </div>
+
+                <div className='empty'></div>
             </div>
         </>
     )
