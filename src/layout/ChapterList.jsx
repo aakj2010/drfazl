1d620593e79230c02b1bad489b82317bef1cf8ef
aakj2010 <aakj2010@gmail.com>
2023-06-30 08:48:28 +0530

    fontSize fixed for ChapterList

--- src/layout/ChapterList.jsx
+++ src/layout/ChapterList.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 import LanguageContext from '../context/LanguageContext';
 import engQuranData from '../screen/eng-quran.json';
 import tamQuranData from '../Tamil Quran/tam-quran.json';
+import FontContext from '../context/FontContext';
 
 const ChapterList = () => {
+    let fontSizeContext = useContext(FontContext)
   const Context = useContext(LanguageContext);
   const data = Context.language === 'Tamil' ? tamQuranData : engQuranData;
   const chapters = data.chapters;
@@ -20,7 +22,7 @@ const ChapterList = () => {
     <div className='cl-wrapper'>
       <div className='cl-header'>
         <div className='cl-header-title'>
-          <h3 className=''>Chapter list</h3>
+          <h3 className='' >Chapter list</h3>
           <h3 className='cl-header-title-length'>{chapters.length}</h3>
         </div>
         <div className='cl-header-closebtn'>
@@ -34,10 +36,10 @@ const ChapterList = () => {
       <div className='cl-list-item-wrapper'>
         {chapters.map((chapter, index) => (
           <div className='cl-list-item' key={index}>
-            <p className='cl-list-item-title'>
+            <p className='cl-list-item-title' style={{ fontSize: `${fontSizeContext.fontSize}px` }}>
               {`${index + 1}. ${chapter.title}`}
             </p>
-            <p className='cl-list-item-length'>{chapter.length}</p>
+            <p className='cl-list-item-length'style={{ fontSize: `${fontSizeContext.fontSize}px` }}>{chapter.length}</p>
           </div>
         ))}
       </div>
