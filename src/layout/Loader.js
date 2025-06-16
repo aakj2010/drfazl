407dcecfb35236cab5e88c576537c4fde0e3ce77
Abdul <abdul.jailani@testsigma.com>
2025-06-16 09:50:46 +0530

    Revert "Migrated CRA to Vite"
    This reverts commit e873a88cdf76df049b95eba6a0cdd34978e98429.

--- src/layout/Loader.js
+++ src/layout/Loader.js
@@ -1,5 +1,7 @@
 export default function Loader() {
     return (
+        // <div className="loader"></div>
+        /* From Uiverse.io by Cybercom682 */
         <div className="text-center mt-32">
             <div
                 className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-primary800 mx-auto"
